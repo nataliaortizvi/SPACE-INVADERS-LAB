4c9be0dde14554257ef3c63f328d81ca8744a018
natalia ortiz <nataliaortiz@MacBook-Air-de-natalia-3.local>
2020-03-05 12:54:57 -0500

    cree empaques con sus clases

---
+++ space invaders Labbb/src/View/Main.java
@@ -0,0 +1,24 @@
+package View;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("View.Main");
+	}
+	public void settings () {
+		size (500, 500);
+		
+	}
+	
+	public void setup () {
+		
+	}
+	
+	public void draw () {
+	background(120,180,350);
+	}
+		
+}
